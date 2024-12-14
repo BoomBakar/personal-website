6b80144bbc1da1a2c11b83615d91f0b3ebe7b41b
BoomBakar <abubakardar608@gmail.com>
2024-12-14 13:38:55 +0500

    Adding linkedin link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/abubakar-dar/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My LinkedIn Profile link
         </a>
       </header>
     </div>
